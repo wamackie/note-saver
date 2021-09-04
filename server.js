11eab1cf44352835ae8a0374818eb16e2401117a
Wamackie <wamackie8456@gmail.com>
2021-09-04 19:47:06 -0400

    working notesArray displayed on server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const { notesArray } = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +9,43 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// function findNoteID(id, notesArray) {
+//     const result = notesArray.filter(notes => notes.id === id)[0];
+//     return result;
+// }
+
+// function createNewNote(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//         path.join(__dirname, './db/db.json'),
+//         JSON.stringify({ notes: notesArray}, null, 2)
+//     );
+//     return note;
+// }
+
+app.get('/api/notes', (req, res) => {
+    let results = notesArray;
+    if (req.query) {
+        results = (req.query, results);
+    }
+    res.json(results);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notesArray);
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
+})
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notesArray.length.toString();
+})
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/index.html'));
